5842e623e37555e43e66c9c194791aac6f06a5b6
requires.io <support@requires.io>
2020-09-07 10:06:02 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     zip_safe=False,
     setup_requires=[],
     install_requires=[
-        'deform >=2.0.3, <2.0.11',
+        'deform>=2.0,<2.1',
         'fanstatic',
         'js.jquery',
         'js.jquery_form',
